3f3bbf46898b90ff9e98758607a191f995afe8e0
thaker0503 <thaker.yatharth.05@gmail.com>
2024-07-15 16:45:05 +0530

    Update medusa-config.js file

--- medusa-config.js
+++ medusa-config.js
@@ -19,7 +19,7 @@ switch (process.env.NODE_ENV) {
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {}
+} catch (e) { }
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
@@ -47,9 +47,9 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
+      // develop: {
+      //   open: process.env.OPEN_BROWSER !== "false",
+      // },
     },
   },
 ];
@@ -76,6 +76,12 @@ const projectConfig = {
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
+  database_extra: process.env.NODE_ENV !== "development" ?
+    {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    } : {},
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
 };
